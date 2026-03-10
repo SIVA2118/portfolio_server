adc328faaa0cb72f96901dfb38ad7c483d72e126
SIVAKUMAR S <122508923+SIVA2118@users.noreply.github.com>
2026-03-10 18:58:53 +0530

    Add files via upload

--- models/Skill.js
+++ models/Skill.js
@@ -4,6 +4,7 @@ const SkillSchema = new mongoose.Schema({
     name: { type: String, required: true },
     level: { type: Number, required: true },
     icon: { type: String },
+    description: { type: String, default: '' },
     order: { type: Number, default: 0 }
 });
 
